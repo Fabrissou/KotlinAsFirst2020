b0a14499ba295f73fb245826a5e37c6fca40e644
Fabrissou <shulga-2015@bk.ru>
2020-10-22 20:39:56 +0300

    les 5

--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -215,17 +215,10 @@ class Tests {
     @Tag("4")
     fun findCheapestStuff() {
         assertNull(
-            findCheapestStuff(
-                mapOf("Мария" to ("печенье" to 20.0), "Орео" to ("печенье" to 100.0)),
-                "торт"
-            )
-        )
-        assertEquals(
-            "Мария",
-            findCheapestStuff(
-                mapOf("Мария" to ("печенье" to 20.0), "Орео" to ("печенье" to 100.0)),
-                "печенье"
-            )
+                findCheapestStuff(
+                        mapOf("" to ("" to 1.7976931348623157e+308)),
+                        ""
+                )
         )
     }
 
