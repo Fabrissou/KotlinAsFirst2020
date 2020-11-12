51b8e416d1705baf81b4027a04a5e0edf7540a3a
Fabrissou <zasdqetl213401>
2020-11-12 10:39:05 +0300

    7

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -122,7 +122,22 @@ fun sibilants(inputName: String, outputName: String) {
  *
  */
 fun centerFile(inputName: String, outputName: String) {
-    TODO()
+    val writer = File(outputName).bufferedWriter()
+    var maxLine = 0
+    File(inputName).forEachLine {
+        println(it.length)
+        if (it.length > maxLine) maxLine = it.length
+    }
+    print(maxLine)
+    writer.use {
+        File(inputName).forEachLine {line ->
+            for (i in 1..(maxLine - line.trim().length)/2) {
+                it.write(" ")
+            }
+            writer.write(line.trim())
+            writer.newLine()
+        }
+    }
 }
 
 /**
@@ -312,59 +327,60 @@ Suspendisse ~~et elit in enim tempus iaculis~~.
 
 //Функция проходит по всем строкам, берет каждое слово, разбивает его на две части
 //и записывает в writer сначала преобразованную левую часть, а потом преобразованную правую часть.
-fun leftString(a: String): String {
-    var string = a
-    val pattern1 = Regex("\\*\\*\\*")
-    val pattern2 = Regex("\\*\\*")
-    val pattern3 = Regex("\\*")
-    val pattern4 = Regex("~~")
-
-    if (pattern4.containsMatchIn(string)) string = pattern4.replace(string, "<s>")
-    if (pattern1.containsMatchIn(string)) return pattern1.replace(string, "<b><i>")
-    if (pattern2.containsMatchIn(string)) return pattern2.replace(string, "<b>")
-    if (pattern3.containsMatchIn(string)) return pattern3.replace(string, "<i>")
-    return string
-}
-fun rightString(a: String): String {
-    var string = a
-    val pattern1 = Regex("\\*\\*\\*")
-    val pattern2 = Regex("\\*\\*")
-    val pattern3 = Regex("\\*")
-    val pattern4 = Regex("~~")
-
-    if (pattern4.containsMatchIn(string)) string = pattern4.replace(string, "</s>")
-    if (pattern1.containsMatchIn(string)) return pattern1.replace(string, "</b></i>")
-    if (pattern2.containsMatchIn(string)) return pattern2.replace(string, "</b>")
-    if (pattern3.containsMatchIn(string)) return pattern3.replace(string, "</i>")
-    return string
-}
+//fun leftString(a: String): String {
+//    var string = a
+//    val pattern1 = Regex("\\*\\*\\*")
+//    val pattern2 = Regex("\\*\\*")
+//    val pattern3 = Regex("\\*")
+//    val pattern4 = Regex("~~")
+//
+//    if (pattern4.containsMatchIn(string)) string = pattern4.replace(string, "<s>")
+//    if (pattern1.containsMatchIn(string)) return pattern1.replace(string, "<b><i>")
+//    if (pattern2.containsMatchIn(string)) return pattern2.replace(string, "<b>")
+//    if (pattern3.containsMatchIn(string)) return pattern3.replace(string, "<i>")
+//    return string
+//}
+//fun rightString(a: String): String {
+//    var string = a
+//    val pattern1 = Regex("\\*\\*\\*")
+//    val pattern2 = Regex("\\*\\*")
+//    val pattern3 = Regex("\\*")
+//    val pattern4 = Regex("~~")
+//
+//    if (pattern4.containsMatchIn(string)) string = pattern4.replace(string, "</s>")
+//    if (pattern1.containsMatchIn(string)) return pattern1.replace(string, "</b></i>")
+//    if (pattern2.containsMatchIn(string)) return pattern2.replace(string, "</b>")
+//    if (pattern3.containsMatchIn(string)) return pattern3.replace(string, "</i>")
+//    return string
+//}
 fun markdownToHtmlSimple(inputName: String, outputName: String) {
-    val writer = File(outputName).bufferedWriter()
-    writer.use {
-        it.write("<html><body>")
-        it.write("<p>")
-
-        File(inputName).forEachLine {line ->
-            if (line.isNotEmpty()) {
-                line.split(' ').forEachIndexed {i, el ->
-                    val left = el.substring(0, (el.length / 2))
-                    val right = el.substring(el.length / 2)
-                    if (('*' in left) || ("~~" in left)) it.write(leftString(left)) else
-                        it.write(left)
-                    if (('*' in right) || ("~~" in right)) it.write(rightString(right)) else
-                        it.write(right)
-                    it.write(" ")
-                }
-                it.newLine()
-            } else {
-                it.write("</p>")
-                it.newLine()
-                it.write("<p>")
-            }
-        }
-        it.write("</p>")
-        it.write("</body></html>")
-    }
+//    val writer = File(outputName).bufferedWriter()
+//    writer.use {
+//        it.write("<html><body>")
+//        it.write("<p>")
+//
+//        File(inputName).forEachLine {line ->
+//            if (line.isNotEmpty()) {
+//                line.split(' ').forEachIndexed {i, el ->
+//                    val left = el.substring(0, (el.length / 2))
+//                    val right = el.substring(el.length / 2)
+//                    if (('*' in left) || ("~~" in left)) it.write(leftString(left)) else
+//                        it.write(left)
+//                    if (('*' in right) || ("~~" in right)) it.write(rightString(right)) else
+//                        it.write(right)
+//                    it.write(" ")
+//                }
+//                it.newLine()
+//            } else {
+//                it.write("</p>")
+//                it.newLine()
+//                it.write("<p>")
+//            }
+//        }
+//        it.write("</p>")
+//        it.write("</body></html>")
+//    }
+    TODO()
 }
 
 /**
