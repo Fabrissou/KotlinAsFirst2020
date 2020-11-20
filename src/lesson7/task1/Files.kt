9c9010fa35eba09728137fb21fbffb2539432f59
Fabrissou <shulga-2015@bk.ru>
2020-11-21 00:35:06 +0300

    lesssoooon 7edit

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -328,86 +328,91 @@ Suspendisse ~~et elit in enim tempus iaculis~~.
 fun markdownToHtmlSimple(inputName: String, outputName: String) {
     val steck = mutableListOf<String>("")
     val writer = File(outputName).bufferedWriter()
+
     writer.use {
-        var stringCounter = 0
-        it.write("<html><body>")
-        File(inputName).forEachLine {element ->
-            val line = element.trim()
-            if ((stringCounter == 0) && (line.isNotEmpty())) {
-                it.write("<p>")
-                steck.add("<p>")
-            }
-            if (line.length == 1) {
-                stringCounter++
-                if (line == "*") {
-                    if (steck.last() == "<i>") {
-                        it.write("</i>")
-                        steck.removeAt(steck.size - 1)
-                    } else {
-                        it.write("<i>")
-                        steck.add("<i>")
-                    }
-                } else it.write(line)
-            } else {
-                if (line.isNotEmpty()) {
+        if (File(inputName).length() != 0.toLong()) {
+            var stringCounter = 0
+            it.write("<html><body>")
+            File(inputName).forEachLine {element ->
+                val line = element.trim()
+                if ((stringCounter == 0) && (line.isNotEmpty())) {
+                    it.write("<p>")
+                    steck.add("<p>")
+                }
+                if (line.length == 1) {
                     stringCounter++
-                    var i = 0
-                    while (i < line.lastIndex) {
-                        if (((line[i] == '*') && (line[i + 1] == '*')) && (steck.last() == "<b>")) {
-                            it.write("</b>")
-                            steck.removeAt(steck.size - 1)
-                            i += 2
-                        } else if ((line[i] == '*') && (line[i + 1] == '*')) {
-                            it.write("<b>")
-                            steck.add("<b>")
-                            i += 2
-                        } else if ((line[i] == '*') && (steck.last() == "<i>")) {
+                    if (line == "*") {
+                        if (steck.last() == "<i>") {
                             it.write("</i>")
                             steck.removeAt(steck.size - 1)
-                            i++
-                        } else if (line[i] == '*') {
+                        } else {
                             it.write("<i>")
                             steck.add("<i>")
-                            i++
-                        } else if (((line[i] == '~') && (line[i + 1] == '~')) && (steck.last() == "<s>")) {
-                            it.write("</s>")
+                        }
+                    } else it.write(line)
+                } else {
+                    if (line.isNotEmpty()) {
+                        stringCounter++
+                        var i = 0
+                        while (i < line.lastIndex) {
+                            if (((line[i] == '*') && (line[i + 1] == '*')) && (steck.last() == "<b>")) {
+                                it.write("</b>")
+                                steck.removeAt(steck.size - 1)
+                                i += 2
+                            } else if ((line[i] == '*') && (line[i + 1] == '*')) {
+                                it.write("<b>")
+                                steck.add("<b>")
+                                i += 2
+                            } else if ((line[i] == '*') && (steck.last() == "<i>")) {
+                                it.write("</i>")
+                                steck.removeAt(steck.size - 1)
+                                i++
+                            } else if (line[i] == '*') {
+                                it.write("<i>")
+                                steck.add("<i>")
+                                i++
+                            } else if (((line[i] == '~') && (line[i + 1] == '~')) && (steck.last() == "<s>")) {
+                                it.write("</s>")
+                                steck.removeAt(steck.size - 1)
+                                i += 2
+                            } else if ((line[i] == '~') && (line[i + 1] == '~')) {
+                                it.write("<s>")
+                                steck.add("<s>")
+                                i += 2
+                            } else {
+                                it.write(line[i].toString())
+                                i++
+                            }
+                        }
+                        if ((line.last() == '*') && (line[line.lastIndex - 1] == '*') && (line[line.lastIndex - 2] == '*') && (steck.last() == "<i>")) {
+                            it.write("</i>")
                             steck.removeAt(steck.size - 1)
-                            i += 2
-                        } else if ((line[i] == '~') && (line[i + 1] == '~')) {
-                            it.write("<s>")
-                            steck.add("<s>")
-                            i += 2
-                        } else {
-                            it.write(line[i].toString())
-                            i++
+                        } else if ((line.last() == '*') && (line[line.lastIndex - 1] == '*') && (line[line.lastIndex - 2] == '*') && (steck.last() != "<i>")) {
+                            it.write("<i>")
+                            steck.add("<i>")
+                        } else if ((line.last() == '*') && (line[line.lastIndex - 1] != '*') && (steck.last() == "<i>")) {
+                            it.write("</i>")
+                            steck.removeAt(steck.size - 1)
+                        } else if (((line.last() == '*') && (line[line.lastIndex - 1] != '*') && (steck.last() != "<i>"))) {
+                            it.write("<i>")
+                            steck.add("<i>")
+                        } else if (line.last() != '*') {
+                            if (line.last() == '~') {
+                                if (line[line.lastIndex - 1] != '~') it.write(line.last().toString())
+                            } else it.write(line.last().toString())
                         }
                     }
-                    if ((line.last() == '*') && (line[line.lastIndex - 1] == '*') && (line[line.lastIndex - 2] == '*') && (steck.last() == "<i>")) {
-                        it.write("</i>")
+                    else if ((steck.last() == "<p>") && (stringCounter > 0)) {
+                        it.write("</p>")
+                        stringCounter = 0
                         steck.removeAt(steck.size - 1)
-                    } else if ((line.last() == '*') && (line[line.lastIndex - 1] == '*') && (line[line.lastIndex - 2] == '*') && (steck.last() != "<i>")) {
-                        it.write("<i>")
-                        steck.add("<i>")
-                    } else if ((line.last() == '*') && (line[line.lastIndex - 1] != '*') && (steck.last() == "<i>")) {
-                        it.write("</i>")
-                        steck.removeAt(steck.size - 1)
-                    } else if (((line.last() == '*') && (line[line.lastIndex - 1] != '*') && (steck.last() != "<i>"))) {
-                        it.write("<i>")
-                        steck.add("<i>")
-                    } else if (line.last() != '*') {
-                        if (line.last() == '~') {
-                            if (line[line.lastIndex - 1] != '~') it.write(line.last().toString())
-                        } else it.write(line.last().toString())
                     }
                 }
-                else if ((steck.last() == "<p>") && (stringCounter > 0)) {
-                    it.write("</p>")
-                    stringCounter = 0
-                    steck.removeAt(steck.size - 1)
-                }
             }
-        }
-        it.write("</body></html>")
+            if (steck.last() == "<p>") it.write("</p>")
+            it.write("</body></html>")
+        } else it.write("<html><body><p></p></body></html>")
+
     }
 }
 
