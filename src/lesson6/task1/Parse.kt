c567ee18b4e37a8980f6f70543e745c20731e86d
Fabrissou <shulga-2015@bk.ru>
2020-11-28 01:00:24 +0300

    lesssoooon 6edit

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -237,10 +237,10 @@ fun plusMinus(expression: String): Int {
  */
 fun firstDuplicateIndex(str: String): Int {
     var parts = str.split(" ")
-    if (parts.size <= 2) return if ((parts.size == 2) && (parts[0] == parts[1])) 0 else -1
+    if (parts.size <= 2) return -1
         else {
         var index = 0
-        for (i in 0..parts.size - 1) {
+        for (i in 0..parts.size - 2) {
             if (parts[i].toLowerCase() == parts[i + 1].toLowerCase()) return index
             index += parts[i].length + 1
         }
